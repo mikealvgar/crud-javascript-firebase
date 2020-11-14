34d4fe82dd5b95c784aea89dd16f2dd0666e6ad2
mikedarconde <miguel.ag.123@hotmail.com>
2020-11-13 18:47:20 -0600

    CRUD con javascript y firebase

---
+++ index.js
@@ -0,0 +1,93 @@
+const db = firebase.firestore();
+
+const taskform = document.getElementById('task-form');
+const taskContainer = document.getElementById('tasks-container');
+
+let editStatus = false;
+let id = '';
+
+const saveTask = (title, description) =>
+    db.collection('tasks').doc().set({
+        title,
+        description
+    });
+
+const getTasks = () => db.collection('tasks').get();
+
+const getTask = (id) => db.collection('tasks').doc(id).get();
+
+const onGetTasks = (callback) => db.collection('tasks').onSnapshot(callback);
+
+const deleteTask = id => db.collection('tasks').doc(id).delete();
+
+const updateTask = (id, updateTask) => db.collection('tasks').doc(id).update(updateTask);
+
+window.addEventListener('DOMContentLoaded', async (e) => {
+    onGetTasks((querySnapshot) => {
+        taskContainer.innerHTML = '';
+        querySnapshot.forEach(doc => {
+            console.log(doc.data())
+
+            const task = doc.data();
+            task.id = doc.id;
+            console.log(task);
+            taskContainer.innerHTML += `<div class="card card-body mt-2 border-primary">
+            <h3 class="h5">${task.title}</h3>
+            <p>${task.description}</p>
+            <div>
+                <button class="btn btn-primary btn-delete" data-id="${task.id}">Delete</button>
+                <button class="btn btn-secondary btn-edit" data-id="${task.id}" >Edit</button>
+            </div>
+          </div>`;
+
+           const btnsDelete = document.querySelectorAll('.btn-delete');
+           btnsDelete.forEach(btn => {
+               btn.addEventListener('click', async (e) => {
+                   await deleteTask(e.target.dataset.id)
+               })
+           });
+
+           const btnsEdit = document.querySelectorAll('.btn-edit');
+           btnsEdit.forEach(btn => {
+               btn.addEventListener('click', async (e) => {
+                   const doc  = await getTask(e.target.dataset.id);
+                    console.log(doc.data());
+
+                    const task = doc.data();
+
+                    editStatus = true;
+                    id = doc.id;
+
+                    taskform['task-title'].value = task.title;
+                    taskform['task-description'].value = task.description;
+                    taskform['btn-task-form'].innerText = 'Update';
+               })
+           })
+        })
+    })
+})
+
+
+taskform.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const title = taskform['task-title'];
+    const description = taskform['task-description'];
+
+    if(!editStatus){
+        await saveTask(title.value, description.value);
+    } else {
+        await updateTask(id, {
+            title: title.value,
+            description: description.value
+        });
+
+        editStatus = false;
+        id = '';
+        taskform['btn-task-form'].innerText = 'Save';
+    }
+
+    await getTasks();
+    taskform.reset();
+    title.focus();
+})
